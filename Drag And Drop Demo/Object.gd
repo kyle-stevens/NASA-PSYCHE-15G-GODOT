ed034d0da418ebea0c89d1ab9da377ea2a4a788c
kyle <kylebstev@gmail.com>
2021-02-12 12:40:56 -0700

    Resetting

---
+++ Drag And Drop Demo/Object.gd
@@ -0,0 +1,45 @@
+extends Node2D
+
+var selected = false
+var rest_point
+var rest_nodes = []
+
+export(int, "Object 1", "Object 2") var object_id
+onready var anim = get_node("AnimatedSprite")
+
+
+func _ready():
+	rest_nodes = get_tree().get_nodes_in_group("zone")
+	rest_point = rest_nodes[0].global_position
+	rest_nodes[0].select()
+	if object_id == 0:
+		anim.play("object1")
+	elif object_id == 1:
+		anim.play("object2")
+		anim.flip_h = true
+		anim.flip_v = true
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("click"):
+		selected = true
+		
+func _physics_process(delta):
+	if selected:
+		global_position = lerp(global_position, get_global_mouse_position(), 25 * delta)
+#		look_at(get_global_mouse_position())
+	else:
+		global_position = lerp(global_position, rest_point, 10 * delta)
+#		rotation = lerp_angle(rotation, 0, 10 * delta)
+		
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and not event.pressed:
+			selected = false
+			var shortest_dist = 75
+			for child in rest_nodes:
+				var distance = global_position.distance_to(child.global_position)
+				if distance < shortest_dist and  object_id == child.object_id:
+					child.select()
+					rest_point = child.global_position
+					shortest_dist = distance
+
---
+++ Drag And Drop Demo/Zones.gd
@@ -0,0 +1,21 @@
+extends Position2D
+
+signal correct()
+export var correct = false
+export(int, "Object 1", "Object 2") var object_id
+
+func _draw():
+	draw_circle(Vector2.ZERO, 75, Color.blanchedalmond)
+	modulate.a = 0.05
+	
+func select():
+	for child in get_tree().get_nodes_in_group("zone"):
+		child.deselect()
+	modulate = Color.webmaroon
+	modulate.a = 0.05
+	if correct:
+		emit_signal("correct")
+	
+func deselect():
+	modulate = Color.white
+	modulate.a = 0.05
